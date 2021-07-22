3300ea752812c6acef90b2e361cbc1b6cdb8a626
EerieAlchemist <66958528+EerieAlchemist@users.noreply.github.com>
2021-07-21 23:18:54 -0400

    elixir privacy policy added

--- scripts/interface.js
+++ scripts/interface.js
@@ -1,4 +1,4 @@
-function myFunction() {
+function showInterface() {
     const x = document.getElementById("navDemo");
     if (x.className.indexOf("w3-show") === -1) {
         x.className += " w3-show";
